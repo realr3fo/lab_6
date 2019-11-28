a8bd7d69abe31491d43b338c0847a51314048e98
refoilmiya <refo.ilmiya@gmail.com>
2019-11-29 00:18:49 +0700

    finished implemented addition calculator with JNI

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/refo_ilmiya_akbar/lab_6/MainActivity.java
@@ -0,0 +1,51 @@
+package id.ac.ui.cs.mobileprogramming.refo_ilmiya_akbar.lab_6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editTextNumberA, editTextNumberB;
+    private TextView result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextNumberA = findViewById(R.id.numberA);
+        editTextNumberB = findViewById(R.id.numberB);
+        Button calculate = findViewById(R.id.calculate);
+        result = findViewById(R.id.result);
+
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (editTextNumberA.getText().toString().equals("")) {
+                    editTextNumberA.requestFocus();
+                    editTextNumberA.setError("Fill this field");
+                }
+                if (editTextNumberB.getText().toString().equals("")) {
+                    editTextNumberB.requestFocus();
+                    editTextNumberB.setError("Fill this field");
+                }
+                String numberA = editTextNumberA.getText().toString();
+                String numberB = editTextNumberB.getText().toString();
+                if (!numberA.equals("") && !numberB.equals("")) {
+                    result.setText("Calculated from JNI: " +
+                            stringFromJNI(Integer.parseInt(numberA), Integer.parseInt(numberB)));
+                }
+            }
+        });
+    }
+
+    public native int stringFromJNI(int numberA, int numberB);
+
+    static {
+        System.loadLibrary("helloworld");
+    }
+}
